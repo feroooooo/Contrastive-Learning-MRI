aa82fb59f60924ef8dc5ef336efa62582c37d6a4
feroooooo <1799363617@qq.com>
2024-02-20 20:25:32 +0800

    update train

--- train.py
+++ train.py
@@ -1,11 +1,13 @@
 import torch.optim as optim
 import torch
 import torch.nn as nn
+import os
 from torch.utils.data import DataLoader, random_split
 from torchvision import transforms
 from mri_dataset import ADNIDataset
 from model import Simple3DCNN
 
+# 训练
 def train(model, device, train_loader, optimizer, criterion, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
@@ -16,10 +18,16 @@ def train(model, device, train_loader, optimizer, criterion, epoch):
         loss.backward()
         optimizer.step()
         if batch_idx % 100 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
+            print(f'\rEpoch: {epoch}\t{batch_idx * len(data):>4} / {len(train_loader.dataset):<4} ({100. * batch_idx / len(train_loader):.0f}%)\tLoss: {loss.item():.6f}', end='')
+    print(f'\rEpoch: {epoch}\t{len(train_loader.dataset):>4} / {len(train_loader.dataset):<4} ({100.:.0f}%)\tLatest Loss: {loss.item():.6f}')
 
+
+# 最优损失函数
+best_loss = float("inf")
+# 最优准确率
+best_accuracy = 0
+
+# 测试
 def test(model, device, test_loader, criterion):
     model.eval()
     test_loss = 0
@@ -28,17 +36,46 @@ def test(model, device, test_loader, criterion):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += criterion(output, target).item()
+            test_loss += criterion(output, target).item() * data.size(0)
             pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
     test_loss /= len(test_loader.dataset)
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    accuracy = correct / len(test_loader.dataset)
+    print('Test Dataset:\tAverage Loss: {:.4f}\tAccuracy: {}/{} ({:.1f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+        accuracy * 100.))
+    
+    # 存储模型
+    global best_accuracy
+    global best_loss
+    if best_accuracy < accuracy:
+        best_accuracy = accuracy
+        print('Saving model...')
+        state = {
+            'model': model.state_dict(),
+            'accuracy': accuracy,
+            'loss': test_loss,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, 'checkpoint/best.pth')
+    if best_loss > test_loss:
+        best_loss = test_loss
 
+# 向控制台打印蓝色字符串
+def color_print(str):
+    print(f"\033[94m{str}\033[0m")
+
+
+# 超参数配置
+epoch_num = 5
+batch_size = 8
+learning_rate = 0.0001
 
 if __name__ == "__main__":
+    color_print("Infomations:")
     # 初始化数据集
     data_dir = "D:/Data/MRI/ADNI/Image"
     csv_path = "D:/Data/MRI/ADNI/pheno_ADNI_longitudinal_new.csv"
@@ -48,25 +85,35 @@ if __name__ == "__main__":
 
     # 数据集大小
     dataset_size = len(dataset)
-    train_size = int(dataset_size * 0.85)
-    test_size = dataset_size - train_size
+    train_size = int(dataset_size * 0.7)
+    test_size = int(dataset_size * 0.15)
+    val_size = dataset_size - train_size - test_size
+    print("train size:", train_size)
+    print("test size:", test_size)
+    print("val size:", val_size)
 
     # 数据集切分
-    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+    train_dataset, test_dataset, val_dataset = random_split(dataset, [train_size, test_size, val_size])
 
     # DataLoader
-    batch_size = 4
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
     
     
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print('current device:', device)
     # 实例化网络
     model = Simple3DCNN().to(device)
     # 定义损失函数和优化器
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.0001)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-    for epoch in range(1, 3):  # 总共训练2个epochs
+    color_print("\nStart Training:")
+    for epoch in range(1, epoch_num + 1):  # 总共训练2个epochs
         train(model, device, train_loader, optimizer, criterion, epoch)
-        test(model, device, test_loader, nn.CrossEntropyLoss(reduction='sum'))
+        test(model, device, test_loader, criterion)
+    color_print("Finish Training.")
+    
+    print(f"\nbest loss: {best_loss:.6f}")
+    print(f"best accuracy: {best_accuracy * 100.:.1f}%")
