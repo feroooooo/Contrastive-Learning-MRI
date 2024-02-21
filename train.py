0faa5cf0c47e3ed8b1ec792122bd44d2535cf2be
feroooooo <1799363617@qq.com>
2024-02-21 17:31:41 +0800

    fix val

--- train.py
+++ train.py
@@ -42,6 +42,45 @@ best_loss = float("inf")
 # 最优准确率
 best_accuracy = 0
 
+# 验证
+def validate(model, device, validation_loader, criterion):
+    model.eval()
+    validation_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in validation_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            validation_loss += criterion(output, target).item() * data.size(0)
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    validation_loss /= len(validation_loader.dataset)
+
+    accuracy = correct / len(validation_loader.dataset)
+    print('Validate:\tAverage Loss: {:.4f}\tAccuracy: {}/{} ({:.1f}%)\n'.format(
+        validation_loss, correct, len(validation_loader.dataset), accuracy * 100.))
+    writer.add_scalar("validation loss", validation_loss, epoch)
+    writer.add_scalar("validation accuracy", accuracy, epoch)
+    
+    # 存储模型
+    global best_accuracy
+    global best_loss
+    if best_accuracy < accuracy:
+        best_accuracy = accuracy
+        print('Saving model...')
+        state = {
+            'model': model.state_dict(),
+            'accuracy': accuracy,
+            'loss': validation_loss,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, 'checkpoint/best.pth')
+    if best_loss > validation_loss:
+        best_loss = validation_loss
+        
+        
 # 测试
 def test(model, device, test_loader, criterion):
     model.eval()
@@ -57,46 +96,8 @@ def test(model, device, test_loader, criterion):
     test_loss /= len(test_loader.dataset)
 
     accuracy = correct / len(test_loader.dataset)
-    print('Test Dataset:\tAverage Loss: {:.4f}\tAccuracy: {}/{} ({:.1f}%)\n'.format(
+    print('Test:\tAverage Loss: {:.4f}\tAccuracy: {}/{} ({:.1f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset), accuracy * 100.))
-    writer.add_scalar("test loss", test_loss, epoch)
-    
-    # 存储模型
-    global best_accuracy
-    global best_loss
-    if best_accuracy < accuracy:
-        best_accuracy = accuracy
-        print('Saving model...')
-        state = {
-            'model': model.state_dict(),
-            'accuracy': accuracy,
-            'loss': test_loss,
-            'epoch': epoch,
-        }
-        if not os.path.isdir('checkpoint'):
-            os.mkdir('checkpoint')
-        torch.save(state, 'checkpoint/best.pth')
-    if best_loss > test_loss:
-        best_loss = test_loss
-        
-        
-# 测试
-def validate(model, device, val_loader, criterion):
-    model.eval()
-    val_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in val_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            val_loss += criterion(output, target).item() * data.size(0)
-            pred = output.argmax(dim=1, keepdim=True)
-            correct += pred.eq(target.view_as(pred)).sum().item()
-    val_loss /= len(val_loader.dataset)
-
-    accuracy = correct / len(val_loader.dataset)
-    print('Val Dataset:\tAverage Loss: {:.4f}\tAccuracy: {}/{} ({:.1f}%)\n'.format(
-        val_loss, correct, len(val_loader.dataset), accuracy * 100.))
 
 
 # 向控制台打印蓝色字符串
@@ -105,7 +106,7 @@ def color_print(str):
 
 
 # 超参数配置
-epoch_num = 50
+epoch_num = 10
 batch_size = 8
 learning_rate = 0.0001
 
@@ -122,20 +123,19 @@ if __name__ == "__main__":
     # 数据集大小
     dataset_size = len(dataset)
     train_size = int(dataset_size * 0.7)
-    test_size = int(dataset_size * 0.15)
-    val_size = dataset_size - train_size - test_size
+    validation_size = int(dataset_size * 0.15)
+    test_size = dataset_size - train_size - validation_size
     print("train size:", train_size)
+    print("validation size:", validation_size)
     print("test size:", test_size)
-    print("val size:", val_size)
 
     # 数据集切分
-    train_dataset, test_dataset, val_dataset = random_split(dataset, [train_size, test_size, val_size])
+    train_dataset, validation_dataset, test_dataset = random_split(dataset, [train_size, validation_size, test_size])
 
     # DataLoader
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    validation_loader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-    val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
-    
     
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print('current device:', device)
@@ -148,12 +148,14 @@ if __name__ == "__main__":
     color_print("\nStart Training:")
     for epoch in range(1, epoch_num + 1):  # 总共训练2个epochs
         train(model, device, train_loader, optimizer, criterion, epoch)
-        test(model, device, test_loader, criterion)
+        validate(model, device, validation_loader, criterion)
     
     writer.close()
     time_stop = time.perf_counter()
     color_print("Finish Training.")
     
-    print("total time:{:.3f}s".format(time_stop - time_start))
-    print(f"\nbest loss: {best_loss:.6f}")
-    print(f"best accuracy: {best_accuracy * 100.:.1f}%")
+    print(f"\ntotal time:{time_stop - time_start:.3f}s")
+    print(f"best loss: {best_loss:.6f}")
+    print(f"best accuracy: {best_accuracy * 100.:.1f}%\n")
+    
+    test(model, device, test_loader, criterion)
