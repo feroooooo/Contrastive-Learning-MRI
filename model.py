a01bd70eb24d226ef949fb6c2ed51eec85e43714
feroooooo <1799363617@qq.com>
2024-03-20 18:34:54 +0800

    resnet

--- model.py
+++ model.py
@@ -3,6 +3,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+# input shape: batch_size x 1 x 100 x 100 x 100
+# output shape: batch_size x class_nums
+
+
+# Simple3DCNN
 class Simple3DCNN(nn.Module):
     def __init__(self, class_nums):
         super(Simple3DCNN, self).__init__()
@@ -15,7 +20,7 @@ class Simple3DCNN(nn.Module):
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 12 * 12 * 12)
+        x = torch.flatten(x, 1)
         x = self.fc(x)
         x = self.last_fc(x)
         return x
@@ -23,9 +28,9 @@ class Simple3DCNN(nn.Module):
 
 
 # out_dim为计算损失时的维度，训练完毕推理时输出的维度为dim_mlp
-class Simple3DCNN_SIMCLR(nn.Module):
+class Simple3DCNN_SimCLR(nn.Module):
     def __init__(self, out_dim):
-        super(Simple3DCNN_SIMCLR, self).__init__()
+        super(Simple3DCNN_SimCLR, self).__init__()
         self.backbone = Simple3DCNN(out_dim)
         dim_mlp = self.backbone.last_fc.in_features
         self.backbone.last_fc = nn.Sequential(nn.Linear(dim_mlp, dim_mlp), nn.ReLU(), self.backbone.last_fc)
@@ -36,9 +41,9 @@ class Simple3DCNN_SIMCLR(nn.Module):
 
 
 # VoxVGG
-class VGG3D(nn.Module):
-    def __init__(self):
-        super(VGG3D, self).__init__()
+class VoxVGG(nn.Module):
+    def __init__(self, class_nums):
+        super(VoxVGG, self).__init__()
         
         # Define the layers
         self.conv1 = nn.Conv3d(in_channels=1, out_channels=8, kernel_size=3)
@@ -55,10 +60,9 @@ class VGG3D(nn.Module):
         self.conv9 = nn.Conv3d(64, 64, 3)
         self.conv10 = nn.Conv3d(64, 64, 3)
 
-        self.fc1 = nn.Linear(64, 128)
-        self.bn1 = nn.BatchNorm1d(128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 3) 
+        self.fc = nn.Linear(64, 128)
+        self.bn = nn.BatchNorm1d(128)
+        self.last_fc = nn.Linear(128, class_nums)
 
     def forward(self, x):
         # Define the forward pass
@@ -80,19 +84,29 @@ class VGG3D(nn.Module):
         x = F.relu(self.conv10(x))
         x = F.max_pool3d(x, 2)
 
-        # Flatten the output for the dense layer
         x = torch.flatten(x, 1)
         
-        x = F.dropout(F.relu(self.bn1(self.fc1(x))), 0.7)
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.dropout(F.relu(self.bn(self.fc(x))), 0.7)
+        x = self.last_fc(x)
 
+        return x
+
+
+class VoxVGG_SimCLR(nn.Module):
+    def __init__(self, out_dim):
+        super(VoxVGG_SimCLR, self).__init__()
+        self.backbone = VoxVGG(out_dim)
+        dim_mlp = self.backbone.last_fc.in_features
+        self.backbone.last_fc = nn.Sequential(nn.Linear(dim_mlp, dim_mlp), nn.ReLU(), self.backbone.last_fc)
+
+    def forward(self, x):
+        x = self.backbone(x)
         return x
 
 
 # VoxResNet
 class VoxResNet(nn.Module):
-    def __init__(self):
+    def __init__(self, class_nums):
         super(VoxResNet, self).__init__()
         # Initial Convolution Block
         self.conv1a = nn.Conv3d(1, 32, kernel_size=3, padding=1)
@@ -107,13 +121,13 @@ class VoxResNet(nn.Module):
         self.conv4 = nn.Conv3d(64, 64, kernel_size=3, stride=2, padding=2)
         self.voxres5 = self._voxres_block(64, 64)
         self.voxres6 = self._voxres_block(64, 64)
-        self.conv7 = nn.Conv3d(64, 128, kernel_size=3, stride=2, padding=2)
+        self.conv7 = nn.Conv3d(64, 128, kernel_size=3, stride=2, padding=1)
         self.voxres8 = self._voxres_block(128, 128)
         self.voxres9 = self._voxres_block(128, 128)
         # Final Layers
         # self.pool10 = nn.AdaptiveAvgPool3d((1, 1, 1))
-        self.fc11 = nn.Linear(1024, 128)
-        self.prob = nn.Linear(128, 3)
+        self.fc = nn.Linear(1024, 128)
+        self.last_fc = nn.Linear(128, class_nums)
 
     def _voxres_block(self, in_channels, out_channels):
         layers = nn.Sequential(
@@ -144,26 +158,24 @@ class VoxResNet(nn.Module):
 
         x7 = F.relu(self.conv7(x6))
         x8 = self.voxres8(x7) + x7
-        x9 = self.voxres9(x8) + x8
+        x = self.voxres9(x8) + x8
         # 最大池化
-        x = F.max_pool3d(x9, 8)
+        x = F.max_pool3d(x, 7)
         x = torch.flatten(x, 1)
         # x = self.pool10(x9).view(x9.size(0), -1)
-        x = F.relu(self.fc11(x))
-        x = self.prob(x)
-
+        x = F.relu(self.fc(x))
+        x = self.last_fc(x)
+        
         return x
+    
 
+class VoxResNet_SimCLR(nn.Module):
+    def __init__(self, out_dim):
+        super(VoxResNet_SimCLR, self).__init__()
+        self.backbone = VoxResNet(out_dim)
+        dim_mlp = self.backbone.last_fc.in_features
+        self.backbone.last_fc = nn.Sequential(nn.Linear(dim_mlp, dim_mlp), nn.ReLU(), self.backbone.last_fc)
 
-
-
-if __name__ == '__main__':
-    # net = VGG3D()
-    net = Simple3DCNN(class_nums=3)
-    net.eval()
-    # Generate a random test input tensor of the size (1, 91, 109, 91)
-    test_input = torch.randn(2, 1, 100, 100, 100)
-
-    # Forward pass through the network
-    test_output = net(test_input)
-    print(test_output.shape)
+    def forward(self, x):
+        x = self.backbone(x)
+        return x
---
+++ test.py
@@ -0,0 +1,62 @@
+from model import Simple3DCNN, Simple3DCNN_SimCLR, VoxVGG, VoxVGG_SimCLR, VoxResNet, VoxResNet_SimCLR
+import torch
+
+def net_simple_test():
+    net = Simple3DCNN(class_nums=3)
+    net.eval()
+    test_input = torch.randn(2, 1, 100, 100, 100)
+    test_output = net(test_input)
+    print(test_output.shape)
+    assert test_output.shape == torch.Size([2, 3])
+    
+    
+def net_simplesimclr_test():
+    net = Simple3DCNN_SimCLR(out_dim=1024)
+    net.eval()
+    test_input = torch.randn(2, 1, 100, 100, 100)
+    test_output = net(test_input)
+    print(test_output.shape)
+    assert test_output.shape == torch.Size([2, 1024])
+    
+
+def net_voxvgg_test():
+    net = VoxVGG(class_nums=5)
+    net.eval()
+    test_input = torch.randn(2, 1, 100, 100, 100)
+    test_output = net(test_input)
+    print(test_output.shape)
+    assert test_output.shape == torch.Size([2, 5])
+    
+
+def net_voxvggsimclr_test():
+    net = VoxVGG_SimCLR(out_dim=1024)
+    net.eval()
+    test_input = torch.randn(2, 1, 100, 100, 100)
+    test_output = net(test_input)
+    print(test_output.shape)
+    assert test_output.shape == torch.Size([2, 1024])
+
+
+def net_voxresnet_test():
+    net = VoxResNet(class_nums=5)
+    net.eval()
+    test_input = torch.randn(2, 1, 100, 100, 100)
+    test_output = net(test_input)
+    print(test_output.shape)
+    assert test_output.shape == torch.Size([2, 5])
+    
+
+def net_voxresnetsimclr_test():
+    net = VoxResNet_SimCLR(out_dim=1024)
+    net.eval()
+    test_input = torch.randn(2, 1, 100, 100, 100)
+    test_output = net(test_input)
+    print(test_output.shape)
+    assert test_output.shape == torch.Size([2, 1024])
+
+net_simple_test()
+net_simplesimclr_test()
+net_voxvgg_test()
+net_voxvggsimclr_test()
+net_voxresnet_test()
+net_voxresnetsimclr_test()
